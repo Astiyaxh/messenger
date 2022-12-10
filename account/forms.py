defbf8c08781f85cc96155fc6964788c9b32ccf9
Astiyaxh <ariyan.mehri00@gmail.com>
2022-12-11 01:26:56 +0330

    search_results.html and Create edit_account.html and backend

--- account/forms.py
+++ account/forms.py
@@ -3,13 +3,15 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate
 from account.models import Account
 
+
 class RegisterationForm(UserCreationForm):
 
-    email = forms.EmailField(max_length=255, help_text="Required. Add a valid email address.")
+    email = forms.EmailField(
+        max_length=255, help_text="Required. Add a valid email address.")
 
     class Meta:
-        model = Account
-        fields = ('email', 'username', 'password1', 'password2')
+        model    = Account
+        fields   = ('email', 'username', 'password1', 'password2')
 
     def clean_email(self):
         email = self.cleaned_data['email'].lower()
@@ -33,15 +35,46 @@ class AccountAuthenticationForm(forms.ModelForm):
     password = forms.CharField(label="Password", widget=forms.PasswordInput)
 
     class Meta:
-        model = Account
-        fields = ("email", "password")
+        model   = Account
+        fields  = ("email", "password")
 
     def clean(self):
         if self.is_valid():
-            email = self.cleaned_data['email']
-            password = self.cleaned_data['password']
+            email      = self.cleaned_data['email']
+            password   = self.cleaned_data['password']
             if not authenticate(email=email, password=password):
                 raise forms.ValidationError("Email OR Password is wrong")
 
 
-class AccountUpdateForm(forms.ModelForm)
+class AccountUpdateForm(forms.ModelForm):
+
+    class Meta:
+        model = Account
+        fields = ('username', 'email', 'profile_image', 'hide_email' )
+
+    def clean_email(self):
+        email = self.cleaned_data['email'].lower()
+        try:
+            account = Account.objects.exclude(pk=self.instance.pk).get(email=email)
+        except Account.DoesNotExist:
+            return email
+        raise forms.ValidationError(f'Email {email} is already in use.')
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        try:
+            account = Account.objects.exclude(pk=self.instance.pk).get(username=username)
+        except Account.DoesNotExist:
+            return username
+        raise forms.ValidationError('Username {username} is already in use.')
+
+
+    def save(self, commit=True):
+        account = super(AccountUpdateForm, self).save(commit=False)
+        account.username = self.cleaned_data['username']
+        account.email = self.cleaned_data['email'].lower()
+        account.profile_image = self.cleaned_data['profile_image']
+        account.hide_email = self.cleaned_data['hide_email']
+        if commit:
+            account.save()
+        return 
